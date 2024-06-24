6a08af0a721ad71c16b93b7b60ba30247f9f9110
Diego Andrés Mancilla <163580176+DiegoTechSolutions@users.noreply.github.com>
2024-06-24 12:29:40 -0400

    modificar productos cambiado
    cambiado

---
+++ modificar productos.py
@@ -0,0 +1,11 @@
+productos=[]
+
+
+def modificar_producto():
+    len(productos)
+    modificar=input("¿Que producto desea modificar?")
+    if modificar in productos:
+        productos([modificar])
+    else:
+        print("Este elemento no esta en la lista");
+
